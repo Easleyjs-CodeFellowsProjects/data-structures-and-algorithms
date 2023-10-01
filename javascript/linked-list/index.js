5c494c61a4e2e153486cb314b34faf33e322c808
Josh Easley <easleyjs@gmail.com>
2023-10-01 15:39:46 -0700

    Passes first test - equal length lists

--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -50,6 +50,7 @@ class LinkedList {
     Returns: a string representing all the values in the Linked List, formatted as:
     "{ a } -> { b } -> { c } -> NULL"
   */
+  /*
   toString() {
     let currentNode = this.head;
     let listAscii = '';
@@ -62,5 +63,37 @@ class LinkedList {
     return listAscii;
   }
 }
+*/
+  toString() {
+    let currentNode = this.head;
+    let listAscii = '';
 
-module.exports = { LinkedList };
+    while (currentNode) {
+      listAscii = `${listAscii}{ ${currentNode.value} } -> `; // -> ${listAscii}`;
+      currentNode = currentNode.next;
+    }
+    listAscii = `${listAscii}NULL`;
+    return listAscii;
+  }
+}
+
+
+function zipLists(list1, list2) {
+  const zippedList = new LinkedList;
+  let currentNode1 = list1.head;
+  let currentNode2 = list2.head;
+
+  //might need .next on these nodes, we'll see.
+  while ( currentNode1 || currentNode2 ) {
+    if ( currentNode2 ) { zippedList.insert( currentNode2.value ); }
+    if ( currentNode1 ) { zippedList.insert( currentNode1.value ); }
+    currentNode1 = currentNode1.next;
+    currentNode2 = currentNode2.next;
+  }
+  return zippedList;
+}
+
+module.exports = {
+  LinkedList,
+  zipLists
+};
