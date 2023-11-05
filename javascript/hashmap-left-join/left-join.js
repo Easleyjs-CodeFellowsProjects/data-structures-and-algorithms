2b2dd685a846326a055a60f79e2fae4227a9a669
Josh Easley <easleyjs@gmail.com>
2023-11-04 21:34:29 -0700

    Adds working functionality

--- javascript/hashmap-left-join/left-join.js
+++ javascript/hashmap-left-join/left-join.js
@@ -1,33 +1,26 @@
-const BinaryTree = require('./BinaryTree/index');
 const { HashTable } = require('./index');
 
-/*
-  TO-DO:
-  - set up input data/hash tables for testing/etc.
-  - look through .keys() of first hash table
-  - for each key, create a string with word.
-  - then using key/word, loop through the second hash table and look for matches. Append each match to the string.
-  - Push finished string to a results array
-  - Return results array
-*/
 function left_join(firstMap, secondMap) {
   const outputArr = []; // array to hold all strings/words with their synonyms and antonyms
   let containsInt = false;
 
   const parseValue = (v) => {
-    let parsedValue = 0;
     try {
-      parsedValue = parseInt( v );
+      if ( isNaN( parseInt( v ))) {
+        return v;
+      }
+      containsInt = true;
       return null;
     } catch(e) {
-      //console.log('VALUE IS NOT AN INT:', v);
-      return parsedValue;
+      return v;
     }
   };
 
   firstMap.keys().forEach(word => {
-    const synonym = String(firstMap.get( word )).split(':')[1]; //parseValue()
-    const antonym = String(secondMap.get( word )).split(':')[1] || 'NULL'; //parseValue()
+    const firstResult = firstMap.get( word );
+    const secondResult = secondMap.get( word );
+    const synonym = parseValue(String( firstResult ).split(':')[1]);
+    const antonym = secondResult ? parseValue(String( secondResult ).split(':')[1]) : 'NULL';
 
     let wordOutput = `${ word } synonym: ${ synonym } antonym: ${ antonym }`;
     outputArr.push( wordOutput );
@@ -40,19 +33,19 @@ function left_join(firstMap, secondMap) {
   }
 }
 
-const hashMapOne = new HashTable(10);
+const hashMapOne = new HashTable(100);
 hashMapOne.set('diligent', 'employed');
 hashMapOne.set('fond', 'enamored');
 hashMapOne.set('guide', 'usher');
 hashMapOne.set('outfit', 'garb');
 hashMapOne.set('wrath', 'anger');
 
-const hashMapTwo = new HashTable(10);
-hashMapOne.set('diligent', 'idle');
-hashMapOne.set('fond', 'averse');
-hashMapOne.set('guide', 'follow');
-hashMapOne.set('flow', 'jam');
-hashMapOne.set('wrath', 'delight');
+const hashMapTwo = new HashTable(100);
+hashMapTwo.set('diligent', 'idle');
+hashMapTwo.set('fond', 'averse');
+hashMapTwo.set('guide', 'follow');
+hashMapTwo.set('flow', 'jam');
+hashMapTwo.set('wrath', 'delight');
 
 console.log(left_join(hashMapOne, hashMapTwo));
 
